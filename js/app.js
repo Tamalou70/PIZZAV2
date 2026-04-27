74f7640d11980c1f53f529091f635e31aea73c1f
Tamalou70 <52508941+Tamalou70@users.noreply.github.com>
2026-04-27 03:51:59 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  if (deliveryDate) deliveryDate.value = todayInput();
-  if (salaryDate) salaryDate.value = todayInput();
-  if (taxDate) taxDate.value = todayInput();
-  autoLogin();
-});
